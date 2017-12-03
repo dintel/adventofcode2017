5186aeceea5ca8b49be8eb505fb9bc85381f7991
Dmitry Zbarski <dmitry.zbarski@datorama.com>
2017-12-03 13:03:54 +0200

    Solution for day 3 part 2

--- day3.go
+++ day3.go
@@ -7,13 +7,17 @@ import (
 	"strings"
 )
 
+type Point struct {
+	x, y int
+}
+
 func Day3(part int, data []byte) {
+	num, err := strconv.Atoi(strings.TrimSpace(string(data)))
+	if err != nil {
+		log.Fatalf("Failed parsing input number %s (%s)", data, err)
+	}
 	switch part {
 	case 1:
-		num, err := strconv.Atoi(strings.TrimSpace(string(data)))
-		if err != nil {
-			log.Fatalf("Failed parsing input number %s (%s)", data, err)
-		}
 		side := int(math.Ceil(math.Sqrt(float64(num))))
 		if side%2 == 0 {
 			side++
@@ -29,6 +33,80 @@ func Day3(part int, data []byte) {
 		result := x + y
 		log.Printf("Steps required to get data from cell %d: %d", num, result)
 	case 2:
-		log.Print("Not implemented yet")
+		squares := make(map[Point]int)
+		x := 0
+		y := 0
+		side := 0
+		squares[Point{0, 0}] = 1
+		result := 0
+		for {
+			x++
+			side += 2
+			value := squares[Point{x - 1, y + 1}] + squares[Point{x - 1, y}] + squares[Point{x - 1, y - 1}] +
+				squares[Point{x, y + 1}] + squares[Point{x, y - 1}] +
+				squares[Point{x + 1, y + 1}] + squares[Point{x + 1, y}] + squares[Point{x + 1, y - 1}]
+			if value > num {
+				result = value
+				break
+			}
+			squares[Point{x, y}] = value
+			for i := 1; i < side; i++ {
+				y++
+				value := squares[Point{x - 1, y + 1}] + squares[Point{x - 1, y}] + squares[Point{x - 1, y - 1}] +
+					squares[Point{x, y + 1}] + squares[Point{x, y - 1}] +
+					squares[Point{x + 1, y + 1}] + squares[Point{x + 1, y}] + squares[Point{x + 1, y - 1}]
+				if value > num {
+					result = value
+					break
+				}
+				squares[Point{x, y}] = value
+			}
+			if result != 0 {
+				break
+			}
+			for i := 0; i < side; i++ {
+				x--
+				value := squares[Point{x - 1, y + 1}] + squares[Point{x - 1, y}] + squares[Point{x - 1, y - 1}] +
+					squares[Point{x, y + 1}] + squares[Point{x, y - 1}] +
+					squares[Point{x + 1, y + 1}] + squares[Point{x + 1, y}] + squares[Point{x + 1, y - 1}]
+				if value > num {
+					result = value
+					break
+				}
+				squares[Point{x, y}] = value
+			}
+			if result != 0 {
+				break
+			}
+			for i := 0; i < side; i++ {
+				y--
+				value := squares[Point{x - 1, y + 1}] + squares[Point{x - 1, y}] + squares[Point{x - 1, y - 1}] +
+					squares[Point{x, y + 1}] + squares[Point{x, y - 1}] +
+					squares[Point{x + 1, y + 1}] + squares[Point{x + 1, y}] + squares[Point{x + 1, y - 1}]
+				if value > num {
+					result = value
+					break
+				}
+				squares[Point{x, y}] = value
+			}
+			if result != 0 {
+				break
+			}
+			for i := 0; i < side; i++ {
+				x++
+				value := squares[Point{x - 1, y + 1}] + squares[Point{x - 1, y}] + squares[Point{x - 1, y - 1}] +
+					squares[Point{x, y + 1}] + squares[Point{x, y - 1}] +
+					squares[Point{x + 1, y + 1}] + squares[Point{x + 1, y}] + squares[Point{x + 1, y - 1}]
+				if value > num {
+					result = value
+					break
+				}
+				squares[Point{x, y}] = value
+			}
+			if result != 0 {
+				break
+			}
+		}
+		log.Printf("First square value above %d is %d at (%d,%d)", num, result, x, y)
 	}
 }
